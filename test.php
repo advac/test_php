006e7d0fbf6411e3972807ee3d16c9824be7a865
Advac <48288487+advac@users.noreply.github.com>
2021-03-05 11:40:13 +0100

    3

--- test.php
+++ test.php
@@ -1,15 +1,32 @@
 <?php
-if( isset( $_POST[ 'Login' ] ) ) {
+if( isset( $_POST['Login']) && ($_POST['Login']!="")) {
     $user = $_POST[ 'username' ];
     $pass = $_POST[ 'password' ];
     $pass = bcrypt( $pass );
-    $query  = mysqli_query($mysqli,"select* from user where username='$user' and password='$pass'");
-    $result = mysqli_fetch_array($query);
-    if($result){
-        $html .= "<p>Bienvenue {$user}</p>";
-        $html .= "<img src=\"{$avatar}\" />";
+    $base = mysqli_connect('localhost', 'root', '', 'test');
+    $query  = mysqli_fetch_assoc(mysqli_query($base, "SELECT * FROM `utilisateurs` WHERE `email` = '". $user."'"));
+    print_r($query);
+    if($query){
+        $passwordUser = $query["password"];
+        if($pass == $passwordUser){
+            $prenom = $query["prenom"];
+            $nom = $query["nom"];
+            echo "Bienvenue " . $prenom . " " . $nom;
+            session_start();
+            $_SESSION['prenom'] = $prenom;
+            $_SESSION['nom'] = $nom;
+            $html .= "<img src=\"{$avatar}\" />";
+        }
+        else {
+            echo "Mot de passe incorrect";
+        }
     }
     else {
-        $html .= "<pre><br />Username and/or password incorrect.</pre>";
-    }}
+        echo "Connexion échouée";
+    }
+
+
+} else {
+    echo "Il y a une erreur";
+}
 ?>
